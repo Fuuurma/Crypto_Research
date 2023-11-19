ca126c32aa7481cfea1dcb772091656cf77474e3
fuuurma <sergiformatjer1999@gmail.com>
2023-11-19 21:13:26 +0100

    coins general& hisotric price nb & model

--- backend/models/coin_model.py
+++ backend/models/coin_model.py
@@ -1,43 +1,38 @@
-from app import db
+from app import db_coins
 
-
-class Coin(db.Model):
+class Coin(db_coins.Model):
     __tablename__ = 'coins_general'
-    id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(50))
-    name = db.Column(db.String(100))
-    image = db.Column(db.String(255))
-    current_price = db.Column(db.Float)
-    market_cap = db.Column(db.Float)
-    market_cap_rank = db.Column(db.Integer)
-    fully_diluted_valuation = db.Column(db.Float)
-    total_volume = db.Column(db.Float)
-    high_24h = db.Column(db.Float)
-    low_24h = db.Column(db.Float)
-    price_change_24h = db.Column(db.Float)
-    price_change_percentage_24h = db.Column(db.Float)
-    market_cap_change_24h = db.Column(db.Float)
-    market_cap_change_percentage_24h = db.Column(db.Float)
-    circulating_supply = db.Column(db.Float)
-    total_supply = db.Column(db.Float)
-    max_supply = db.Column(db.Float)
-    ath = db.Column(db.Float)
-    ath_change_percentage = db.Column(db.Float)
-    ath_date = db.Column(db.String(50))
-    atl = db.Column(db.Float)
-    atl_change_percentage = db.Column(db.Float)
-    atl_date = db.Column(db.String(50))
-    last_updated = db.Column(db.String(50))
-    price_change_percentage_14d_in_currency = db.Column(db.Float)
-    price_change_percentage_1h_in_currency = db.Column(db.Float)
-    price_change_percentage_1y_in_currency = db.Column(db.Float)
-    price_change_percentage_200d_in_currency = db.Column(db.Float)
-    price_change_percentage_24h_in_currency = db.Column(db.Float)
-    price_change_percentage_30d_in_currency = db.Column(db.Float)
-    price_change_percentage_7d_in_currency = db.Column(db.Float)
+    id = db_coins.Column(db_coins.Integer, primary_key=True)
+    symbol = db_coins.Column(db_coins.String(50))
+    name = db_coins.Column(db_coins.String(100))
+    image = db_coins.Column(db_coins.String(255))
+    current_price = db_coins.Column(db_coins.Float)
+    market_cap = db_coins.Column(db_coins.Float)
+    fully_diluted_valuation = db_coins.Column(db_coins.Float)
+    total_volume = db_coins.Column(db_coins.Float)
+    high_24h = db_coins.Column(db_coins.Float)
+    low_24h = db_coins.Column(db_coins.Float)
+    price_change_24h = db_coins.Column(db_coins.Float)
+    price_change_percentage_24h = db_coins.Column(db_coins.Float)
+    circulating_supply = db_coins.Column(db_coins.Float)
+    total_supply = db_coins.Column(db_coins.Float)
+    max_supply = db_coins.Column(db_coins.Float)
+    ath = db_coins.Column(db_coins.Float)
+    ath_change_percentage = db_coins.Column(db_coins.Float)
+    ath_date = db_coins.Column(db_coins.String(50))
+    atl = db_coins.Column(db_coins.Float)
+    atl_change_percentage = db_coins.Column(db_coins.Float)
+    atl_date = db_coins.Column(db_coins.String(50))
+    last_updated = db_coins.Column(db_coins.String(50))
+    price_change_1Y = db_coins.Column(db_coins.Float)
+    price_change_200d = db_coins.Column(db_coins.Floaty)
+    price_change_24h = db_coins.Column(db_coins.Float)
+    price_change_30d = db_coins.Column(db_coins.Float)
+    price_change_7d = db_coins.Column(db_coins.Float)
 
     def __repr__(self):
         return f"<Coin: {self.id} ({self.symbol})>"
+
     
     
 # Helper functions
